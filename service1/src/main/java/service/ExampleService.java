145e6954aadb05352ffbc14ef21a924608a5ec9b
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2024-12-14 20:16:28 +0100

    🎉 init

---
+++ service1/src/main/java/service/ExampleService.java
@@ -0,0 +1,26 @@
+package service;
+
+import entity.Example;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import repository.ExampleRepository;
+
+import java.util.List;
+
+@ApplicationScoped
+public class ExampleService {
+
+    @Inject
+    ExampleRepository exampleRepository;
+
+    public Example createExample(String var1, String var2) {
+        Example example = new Example(var1, var2);
+        exampleRepository.persistExample(example);
+        return example;
+
+
+    }
+
+    public List<Example> getAllExamples(){return exampleRepository.listAllExamples();}
+
+}
