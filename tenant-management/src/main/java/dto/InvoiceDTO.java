eca9bd9bf1fddfc340c756b9fc319b1f78cc6372
zom5583 <zom5583@thi.de>
2025-01-03 17:12:26 +0100

    Update InvoiceDTO

--- tenant-management/src/main/java/dto/InvoiceDTO.java
+++ tenant-management/src/main/java/dto/InvoiceDTO.java
@@ -1,41 +1,84 @@
 package dto;
 
+/**
+ * Data Transfer Object (DTO) for Invoice.
+ * This class represents the data structure for an invoice, including the receiver's details,
+ * the invoice amount, and a description of the invoice.
+ */
 public class InvoiceDTO {
     private String receiver;
     private String receiverIban;
     private double invoiceAmount;
     private String description;
 
-    // Getters and Setters
+    /**
+     * Gets the receiver of the invoice.
+     *
+     * @return the receiver of the invoice
+     */
     public String getReceiver() {
         return receiver;
     }
 
+    /**
+     * Sets the receiver of the invoice.
+     *
+     * @param receiver the receiver of the invoice
+     */
     public void setReceiver(String receiver) {
         this.receiver = receiver;
     }
 
+    /**
+     * Gets the IBAN of the receiver.
+     *
+     * @return the IBAN of the receiver
+     */
     public String getReceiverIban() {
         return receiverIban;
     }
 
+    /**
+     * Sets the IBAN of the receiver.
+     *
+     * @param receiverIban the IBAN of the receiver
+     */
     public void setReceiverIban(String receiverIban) {
         this.receiverIban = receiverIban;
     }
 
-
+    /**
+     * Gets the amount of the invoice.
+     *
+     * @return the amount of the invoice
+     */
     public double getInvoiceAmount() {
         return invoiceAmount;
     }
 
+    /**
+     * Sets the amount of the invoice.
+     *
+     * @param invoiceAmount the amount of the invoice
+     */
     public void setInvoiceAmount(double invoiceAmount) {
         this.invoiceAmount = invoiceAmount;
     }
 
+    /**
+     * Gets the description of the invoice.
+     *
+     * @return the description of the invoice
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the description of the invoice.
+     *
+     * @param description the description of the invoice
+     */
     public void setDescription(String description) {
         this.description = description;
     }
