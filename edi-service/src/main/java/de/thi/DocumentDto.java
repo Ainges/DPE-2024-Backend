24084ef0cbd57c6647f685d2fb6a8374883eba21
Leo-uni-0305 <leonie-krauss@web.de>
2025-01-15 14:16:40 +0100

    EDI Service Kommentiert

--- edi-service/src/main/java/de/thi/DocumentDto.java
+++ edi-service/src/main/java/de/thi/DocumentDto.java
@@ -2,16 +2,16 @@ package de.thi;
 
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-
+// Defines the Java class DocumentDto
 public class DocumentDto {
-
+//Shows that invoice is displayed in the XML file as Invoice
     @JacksonXmlProperty(localName = "Invoice")
     private Invoice invoice;
-
+//Shows that digitalInvoice is displayed in the XML file as digitalInvoice
     @JacksonXmlProperty(localName = "digitalInvoice")
     private Boolean digitalInvoice;
 
-    // Getter und Setter
+    // Defines Getter and Setter for invoice and digitalInvoice. Enables access and modification of values
     public Invoice getInvoice() {
         return invoice;
     }
@@ -28,7 +28,10 @@ public class DocumentDto {
         this.digitalInvoice = digitalInvoice;
     }
 
-    // Inneres DTO für Invoice
+    // defines inner static class Invoice with different properties.
+    // Each Property is displayed in the XML file as specified in the @JacksonXmlProperty
+    //The properties include invoiceDate, invoiceAmount, description, status, receiver, receiverIBAN, receiverBIC, currency, and externalInvoiceNumber.
+    // These annotations ensure that when an instance of Invoice is serialized to XML, the properties will be displayed with the specified local names
     public static class Invoice {
 
         @JacksonXmlProperty(localName = "InvoiceDate")
@@ -60,7 +63,9 @@ public class DocumentDto {
 
 
 
-        // Getter und Setter
+        // defines getter and setter methods for the properties of the Invoice inner static class within the DocumentDto class
+        //these methods enable access and modification of the properties of the Invoice class
+        //Each property is annotated with @JacksonXmlProperty to specify how they should be serialized to XML.
         public String getInvoiceDate() {
             return invoiceDate;
         }
